549223d718366dc875935422f569b0862d4b6f68
ArunKumar0707 <115449190+ArunKumar0707@users.noreply.github.com>
2026-05-05 17:08:32 +0530

    Update ProductController.java

--- backend/src/main/java/com/inventory/app/controller/ProductController.java
+++ backend/src/main/java/com/inventory/app/controller/ProductController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/products")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://shiny-stardust-7757fe.netlify.app")
 public class ProductController {
 
     private final ProductService productService;
